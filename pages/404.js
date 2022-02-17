e8b8c85e027dadeb6566891e01b477e95f1dd95a
layin <layon.final008@gmail.com>
2022-02-17 09:54:49 -0300

    Added fetched content from each .md to post

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1>404 - Sauce Not Found</h1>
+      <p>Looks like this page does not exist</p>
+    </Layout>
+  )
+  }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Date from '../components/date'
+
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -27,12 +29,14 @@ export default function Home({ allPostsData }) {
           <ul className={utilStyles.list}>
             {allPostsData.map(({ id, date, title }) => (
               <li className={utilStyles.listItem} key={id}>
-                {title}
-                <br />
-                {id}
-                <br />
-                {date}
-              </li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
             ))}
           </ul>
         </section>
